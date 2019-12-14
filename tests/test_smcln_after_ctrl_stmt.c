51c3baa2aaa02ce3ad2ec43ca13b09d442ad3d0b
kokke <spam@rowdy.dk>
2019-12-14 04:21:33 +0100

    Add files via upload

---
+++ tests/test_smcln_after_ctrl_stmt.c
@@ -0,0 +1,21 @@
+
+
+int main()
+{
+  int x = rand();
+  if (x > 42);
+  {
+    printf("then do something else!");
+  }
+
+  int i;
+  for (i = 0; i < x; ++i); {
+    printf("then do something else!");
+  }
+
+  while (x > 42); {
+    printf("then do something else!");
+  }
+  return 0;
+}
+
