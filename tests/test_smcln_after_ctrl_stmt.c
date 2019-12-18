37bbe94defcb3fdad8cd2b584561d08998e20b36
kokke <spam@rowdy.dk>
2019-12-18 22:40:59 +0100

    Update test_smcln_after_ctrl_stmt.c

--- tests/test_smcln_after_ctrl_stmt.c
+++ tests/test_smcln_after_ctrl_stmt.c
@@ -13,9 +13,27 @@ int main(void)
     printf("then do something else!");
   }
 
+  i = 0;
+  do
+  {
+    i +=1;
+    int j;
+    for (j = 0; j < 5; ++j)
+    {
+      if (    (i > j)
+           && ((i - j) > 1))
+      {
+        printf(".....");
+      }
+      do { printf("."); } while(0);
+    }
+  } while (i < 10);
+
   while (x > 42); { /* HIT */
     printf("then do something else!");
   }
+
+
   return 0;
 }
 
